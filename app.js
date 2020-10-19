dac2a12d922c5e26c5a111e19ba8c88b17e857ee
unknown <tkd.lsanchez@gmail.com>
2020-10-19 16:50:19 -0600

    using event delegation. It makes more sense.

--- app.js
+++ app.js
@@ -1,46 +1,31 @@
-
-function listenForCreateNewQuestion(addQuestionElement) {
-    if (addQuestionElement != null) {
-        addQuestionElement.addEventListener('click', (e) => {
-            // e.preventDefault();
-            e.stopPropagation();
-            renderAdditionalQuestion(document.querySelector('#questions'))
-        });
-    };
-};
-
 function renderAdditionalQuestion(appender) {
-
     // need some way of knowing what question this is numerically. 
     const currentQuestionCount = document.querySelector('#questions').querySelectorAll('.question').length;
-    
-    console.log('currentQuestionCount', currentQuestionCount)
-
     const newQuestionHTMLTemplate = `
 <div class="card col-12 mb-3">
     <div class="card-body">                   
         <div class="form-group col-12">
         <div class="float-right m-1">
-            <button type="button" id="remove-quiz-question-${currentQuestionCount}" class="btn btn-danger">Delete ${currentQuestionCount}</button>
+            <button type="button" class="btn btn-danger delete-question">Delete ${currentQuestionCount}</button>
         </div>
             <label class="card-title font-weight-bold" for="quiz-question-${currentQuestionCount}">Quiz Question ${currentQuestionCount}</label>
             <textarea class="form-control question" id="quiz-question-${currentQuestionCount}" rows="3"></textarea>
 
     </div>
     <div class="form-group col-12">
-        <div>
-            <button type="button" id="add-question-${currentQuestionCount}" class="btn btn-success">Add Answer Option</button>
-        </div>
         <div class="answer-options">
+            <div>
+                <button type="button" class="btn btn-success add-answer-option">Add Answer Option</button>
+            </div>
             <div class="row answer-option">
                 <div class="col-12">
                     <label class="card-subtitle mb-2 font-weight-bold" for="quiz-answer-${currentQuestionCount}">Quiz Answer Option 0</label>
                     </div>
-                    <div class="col-12 col-sm-6 col-md-10">
+                    <div class="col-12 col-sm-6 col-md-9">
                     <input type="text" class="form-control" id="quiz-answer-${currentQuestionCount}">
                 </div>
-                <div class="col-12 col-sm-6 col-md-2">
-                    <button type="button" id="remove-quiz-answer-${currentQuestionCount}" class="btn btn-danger disabled align-self-center">Remove Option</button>
+                <div class="col-12 col-sm-6 col-md-3">
+                    <button type="button" class="btn btn-danger disabled align-self-center remove-answer-option">Remove Option</button>
                 </div>
             </div>
         </div>
@@ -49,64 +34,57 @@ function renderAdditionalQuestion(appender) {
 </div>
 `;
     appender.innerHTML += newQuestionHTMLTemplate;
-    removeQuestion(document.querySelector(`#remove-quiz-question-${currentQuestionCount}`));
-
-    listenForAddAnswerOption(document.querySelector(`#add-question-${currentQuestionCount}`), currentQuestionCount);
 };
 
-function removeQuestion(removeElement) {
-    removeElement.addEventListener('click', (e) => {
-        e.stopPropagation();
-        e.target.closest('.card').remove();
-    })
-    // slice the question out? of the final object array. -why not construct object on submit then use that?
-    // re-order all of the ids?
-};
-
-function listenForAddAnswerOption(addAnswerOptionElement, currentQuestionValue) {
-    if(addAnswerOptionElement != null) {
-        addAnswerOptionElement.addEventListener('click', (e) => {
-            e.stopPropagation();
-            renderAdditionalAnswerOption(e.target.parentElement.parentElement.querySelector('.answer-options'), currentQuestionValue);
-        })
-    }
-}
-
-function renderAdditionalAnswerOption(appender, currentQuestionValue) {
-
+function renderAdditionalAnswerOption(appender) {
     const currentAnswerCount = appender.querySelectorAll('.answer-option').length;
-
     const answerOptionHtmlTemplate = `
     <div class="row answer-option">
                 <div class="col-12">
-                    <label class="card-subtitle mb-2 font-weight-bold" for="quiz-question-${currentQuestionValue}-answer-${currentAnswerCount}">Quiz Answer Option ${currentAnswerCount} </label>
+                    <label class="card-subtitle mb-2 font-weight-bold" for="answer-option-${currentAnswerCount}">Quiz Answer Option ${currentAnswerCount} </label>
                     </div>
-                    <div class="col-12 col-sm-6 col-md-10">
-                    <input type="text" class="form-control" id="quiz-question-${currentQuestionValue}-answer-${currentAnswerCount}">
+                    <div class="col-12 col-sm-6 col-md-9">
+                    <input type="text" class="form-control" id="answer-option-${currentAnswerCount}">
                 </div>
-                <div class="col-12 col-sm-6 col-md-2">
-                    <button type="button" id="remove-quiz-question-${currentQuestionValue}-answer-${currentAnswerCount}" class="btn btn-danger align-self-center">Remove Option ${currentAnswerCount}</button>
+                <div class="col-12 col-sm-6 col-md-3">
+                    <button type="button" class="btn btn-danger justify-content-center align-self-center remove-answer-option">Remove Option ${currentAnswerCount}</button>
                 </div>
             </div>
     `;
-
     appender.innerHTML += answerOptionHtmlTemplate;
-    removeAnswerOption(document.querySelector(`#remove-quiz-question-${currentQuestionValue}-answer-${currentAnswerCount}`));
 };
-
-function removeAnswerOption(removeElement) {
-    if(removeElement != null) {
-        removeElement.addEventListener('click', (e) => {
-            e.stopPropagation();
-            e.target.closest('.answer-option').remove();
-        })
-    }
-}
 
 window.addEventListener("DOMContentLoaded", () => {
 
-    listenForCreateNewQuestion(document.querySelector('#add-question'));
-    listenForAddAnswerOption(document.querySelector(`#add-question-0`));
+    // EVENT DELEGATION
+    document.querySelector('#create-quiz-form').addEventListener("click", function (e) {
+
+        // Adding Questions 
+        if (e.target && e.target.matches("#add-question")) {
+            e.stopPropagation();
+            renderAdditionalQuestion(document.querySelector('#questions'))
+        };
+
+        // Removing Questions
+        if (e.target && e.target.matches('.delete-question')) {
+            e.stopPropagation();
+            e.target.closest('.card').remove();
+        };
+
+        // adding Answer Options
+        if (e.target && e.target.matches('.add-answer-option')) {
+            e.stopPropagation();
+            console.log('hi', e.target)
+            renderAdditionalAnswerOption(e.target.closest('.answer-options'));
+        };
+
+        // Removing Answer Options
+        if (e.target && e.target.matches('.remove-answer-option')) {
+            e.stopPropagation();
+            e.target.closest('.answer-option').remove();
+        };
+
+    });
 
 
 })
