c98d7b90ab0a75597708edc0a024ba7b02da2bb4
unknown <tkd.lsanchez@gmail.com>
2020-10-19 19:45:30 -0600

    Fixed it up so that id's are appropriately updated. Added a correct answer field. set up some validation with that correct answer field. Set it up so on submit an object was created and logged to the console.

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ function renderAdditionalQuestion(appender) {
     // need some way of knowing what question this is numerically. 
     const currentQuestionCount = document.querySelector('#questions').querySelectorAll('.question').length;
     const newQuestionHTMLTemplate = `
-<div class="card col-12 mb-3">
+<div class="card col-12 mb-3 question">
     <div class="card-body">                   
         <div class="form-group col-12">
         <div class="float-right m-1">
             <button type="button" class="btn btn-danger delete-question">Delete ${currentQuestionCount}</button>
         </div>
             <label class="card-title font-weight-bold" for="quiz-question-${currentQuestionCount}">Quiz Question ${currentQuestionCount}</label>
-            <textarea class="form-control question" id="quiz-question-${currentQuestionCount}" rows="3"></textarea>
+            <textarea class="form-control" id="quiz-question-${currentQuestionCount}" rows="3"></textarea>
 
     </div>
     <div class="form-group col-12">
@@ -19,16 +19,22 @@ function renderAdditionalQuestion(appender) {
             </div>
             <div class="row answer-option">
                 <div class="col-12">
-                    <label class="card-subtitle mb-2 font-weight-bold" for="quiz-answer-${currentQuestionCount}">Quiz Answer Option 0</label>
+                    <label class="card-subtitle mb-2 font-weight-bold" for="answer-option-0">Quiz Answer Option 0</label>
                     </div>
                     <div class="col-12 col-sm-6 col-md-9">
-                    <input type="text" class="form-control" id="quiz-answer-${currentQuestionCount}">
+                    <input type="text" class="form-control" id="answer-option-0">
                 </div>
                 <div class="col-12 col-sm-6 col-md-3">
-                    <button type="button" class="btn btn-danger disabled align-self-center remove-answer-option">Remove Option</button>
+                    <button type="button" class="btn btn-danger disabled align-self-center remove-answer-option">Remove Option 0</button>
                 </div>
             </div>
         </div>
+    </div>
+    <div class="form-group col-12">
+        <div class="correct-answer">
+        <label class="font-weight-bold" for="quiz-question-${currentQuestionCount}-correct-answer">Correct Answer for Question ${currentQuestionCount}</label>
+        <input type="text" class="form-control" id="quiz-question-${currentQuestionCount}-correct-answer" >
+    </div>
     </div>
     </div>
 </div>
@@ -57,7 +63,7 @@ function renderAdditionalAnswerOption(appender) {
 window.addEventListener("DOMContentLoaded", () => {
 
     // EVENT DELEGATION
-    document.querySelector('#create-quiz-form').addEventListener("click", function (e) {
+    document.querySelector('#create-quiz-form').addEventListener("click", (e) => {
 
         // Adding Questions 
         if (e.target && e.target.matches("#add-question")) {
@@ -68,23 +74,84 @@ window.addEventListener("DOMContentLoaded", () => {
         // Removing Questions
         if (e.target && e.target.matches('.delete-question')) {
             e.stopPropagation();
-            e.target.closest('.card').remove();
+            e.target.closest('.question').remove();
+            const remainingQuestions = document.querySelectorAll('.question');
+            for (let i = 0; i < remainingQuestions.length; i++) {
+                const question = remainingQuestions[i];
+                question.querySelector('.delete-question').innerText = `Delete ${i}`;
+                question.querySelector('label').innerText = `Quiz Question ${i}`;
+                question.querySelector('label').setAttribute('for', `quiz-question-${i}`);
+                question.querySelector('textarea').setAttribute('id', `quiz-question-${i}`);
+                question.querySelector('.correct-answer label').innerText = `Correct Answer for Question ${i}`;
+                question.querySelector('.correct-answer label').setAttribute('for', `quiz-question-${i}-correct-answer`)
+                question.querySelector('.correct-answer input').setAttribute('id', `quiz-question-${i}-correct-answer`)
+            };
         };
 
         // adding Answer Options
         if (e.target && e.target.matches('.add-answer-option')) {
             e.stopPropagation();
-            console.log('hi', e.target)
             renderAdditionalAnswerOption(e.target.closest('.answer-options'));
         };
 
         // Removing Answer Options
         if (e.target && e.target.matches('.remove-answer-option')) {
             e.stopPropagation();
+            const allOptions = e.target.closest('.answer-options');
             e.target.closest('.answer-option').remove();
+            const remaingAnswerOptions = allOptions.querySelectorAll('.answer-option');
+            for (let i = 0; i < remaingAnswerOptions.length; i++) {
+                const answerOption = remaingAnswerOptions[i];
+                answerOption.querySelector('label').innerText = `Quiz Answer Option ${i}`;
+                answerOption.querySelector('label').setAttribute('for', `answer-option-${i}`);
+                answerOption.querySelector('input').setAttribute('id', `answer-option-${i}`);
+                answerOption.querySelector('.remove-answer-option').innerText = `Remove Option ${i}`;
+            };
         };
 
     });
+
+    document.querySelector('#create-quiz-form').addEventListener('submit', (e) => {
+        e.preventDefault();
+        const newQuizObject = {};
+
+        newQuizObject['quizName'] = document.querySelector('#quiz-name').value;
+
+        const quizQuestions = document.querySelectorAll('.question');
+        for (let i = 0; i < quizQuestions.length; i++) {
+            const currentQuizQuestion = quizQuestions[i];
+
+            newQuizObject[`quizQuestion${i}`] = currentQuizQuestion.querySelector(`#quiz-question-${i}`).value;
+
+            let answerOptionsArray = [];
+            const currentQuestionAnswerOptions = currentQuizQuestion.querySelectorAll('.answer-option');
+            currentQuestionAnswerOptions.forEach(answerOption => {
+                console.log(answerOption)
+                answerOptionsArray.push(answerOption.querySelector('input').value);
+            });
+            newQuizObject[`quizQuestion${i}AnswerOptions`] = answerOptionsArray;
+            newQuizObject[`quizQuestion${i}CorrectAnswer`] = currentQuizQuestion.querySelector('.correct-answer input').value;
+            if(!answerOptionsArray.includes(currentQuizQuestion.querySelector('.correct-answer input').value)) {
+                document.querySelector('#error-message').classList.remove('d-none');
+                document.querySelector('#error-message').innerHTML = '<strong>Error</strong>: One of the answer options must be correct.'
+                return;
+            };
+        };
+
+
+        console.log(newQuizObject);
+        // Then this UI/ UX stuff needs to happen so users don't freak out.
+        document.querySelector('#create-quiz-form').reset();
+        document.querySelector('#success-message').classList.remove('d-none');
+        document.querySelector('#create-quiz-form').classList.add('d-none');
+        document.querySelector('#error-message').classList.add('d-none');
+        document.querySelector('#error-message').innerHTML = '';
+
+        setTimeout(() => {
+            document.querySelector('#success-message').classList.add('d-none');
+            document.querySelector('#create-quiz-form').classList.remove('d-none');
+        },5000)
+    })
 
 
 })
